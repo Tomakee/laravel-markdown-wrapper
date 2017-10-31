a34fd0aec28ccb1fad2adf50489fe04873dae1b2
Tommy <tomyaoki@gmail.com>
2017-10-31 16:47:29 +0900

    Add bootstrap.php for Tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../src/Parser.php';
+require __DIR__.'/../src/MarkdownServiceProvider.php';
+require __DIR__.'/../src/helpers.php';
+require __DIR__.'/../src/Facade/Markdown.php';
+require __DIR__.'/../src/Exceptions/InvalidParserException.php';
+require __DIR__.'/../src/Exceptions/InvalidTagException.php';
+require __DIR__.'/../bootstap/autoload.php';
